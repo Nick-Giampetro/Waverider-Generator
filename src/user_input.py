31c6b10a36fd147fbcd01c0684c98c9571491219
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2025-01-21 17:29:05 -0500

    figured out how this sort of works, made a more limited way to generate waveriders that has more physical intution

--- src/user_input.py
+++ src/user_input.py
@@ -1,45 +1,50 @@
 import numpy as np
 
-#   Calculates Shock Turn Angle Theta
-#   (copied code from oblique_shock.py)
-def theta_calc(m1, gamma, beta):
-    tan_theta = 2 * (1 / np.tan(beta_rad)) * ((m1 ** 2 * np.sin(beta_rad) ** 2 - 1) / (m1 ** 2 * (gamma + np.cos(2 * beta_rad)) + 2))
-    theta_rad = np.atan(tan_theta)
 
-    return theta_rad
+#   Waverider Body Dimensions
+desL = 10   # Length of Body
+W = 10       # Width of Vehicle at the tail end
+H = 10     # Height of Body Over Shock Cone at the rear
 
-#   Waveride Dimensions
-desL = 10  # Length of Body
-H = 3.5 # Height of Body Over Shock Cone at the rear
-W = 9   # Width of Vehicle at the tail end
 
 #   Basic Flowfield
 M1 = 5
 gamma = 1.4
 # beta = 19.29
 # beta_rad = np.radians(beta)
-beta_rad = np.arctan(H/desL)
-beta = (beta_rad * 180)/np.pi
-theta_rad = theta_calc(M1, gamma, beta_rad)
+beta_rad = np.arctan(H / desL)
+beta = (beta_rad * 180) / np.pi
+
+
 
 #   Define TE constants
-L = 18                                # Max. Length (See User Menu)
-Rs = L * np.tan(beta_rad)    # Trailing Edge Parameter (See User Menu)
-R1 = .5 * Rs                        # Trailing Edge Parameter (See User Menu)
-W2 = 0.6429 * Rs                        # Trailing Edge Parameter (See User Menu)
+L = 2*desL  # Max. Length (See User Menu)
+Rs = L * np.tan(beta_rad)  # Trailing Edge Parameter (See User Menu)
+
+# Manual Tuning
+#R1_multiplier = 0.5
+#W2_multiplier = 0.45
+
+# Body dimension Tunner
+R1 = Rs-H
+R1_multiplier = R1/Rs
+W2_multiplier = (W/2)/Rs
+
+R1 = R1_multiplier * Rs  # Trailing Edge Parameter (See User Menu)
+W2 = W2_multiplier * Rs  # Trailing Edge Parameter (See User Menu)
 
 #   Resolution
-N = 500                      # Leading Edge Resolution (500 or more is recommended!)
-N_l = 12                     # Lower Surface Resolution
-N_up = 10                    # Upper Surface Resolution
+N = 500  # Leading Edge Resolution (500 or more is recommended!)
+N_l = 12  # Lower Surface Resolution
+N_up = 10  # Upper Surface Resolution
 
 #   Trailing Edge Curve
 a = -R1
-b = 2 * (R1 - np.sqrt(Rs**2 - W2**2)) / W2**2
-c = (np.sqrt(Rs**2 - W2**2) - R1) / W2**4
+b = 2 * (R1 - np.sqrt(Rs ** 2 - W2 ** 2)) / W2 ** 2
+c = (np.sqrt(Rs ** 2 - W2 ** 2) - R1) / W2 ** 4
+
 
 #   Trailing Edge Function
 def z(y):
-    func = a + b * y**2 + c * y**4    # Define Your Own Trailing Edge Function
+    func = a + b * y ** 2 + c * y ** 4  # Define Your Own Trailing Edge Function
     return func
-
