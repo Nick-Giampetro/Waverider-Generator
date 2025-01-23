73717a600ed61acf449c0be7ecb30c5beba8942d
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2025-01-23 15:56:45 -0500

    new geometry

--- src/user_input.py
+++ src/user_input.py
@@ -2,46 +2,48 @@ import numpy as np
 
 
 #   Waverider Body Dimensions
-desL = 10   # Length of Body (meters)
-W = 8       # Width of Vehicle at the tail end (meters)
-H = 3.5     # Height of Body Over Shock Cone at the rear (meters)
+desL = 3.6  # Length of Body (meters)
+W = 2.6     # Width of Vehicle at the tail end (meters)
+H = 1.7     # Height of Body Over Shock Cone at the rear (meters)
 
 
 #   Basic Flowfield
 M1 = 5
 gamma = 1.4
+
+#   Manual Tuning
 # beta = 19.29
 # beta_rad = np.radians(beta)
+#   Body Dimension Tuner
 beta_rad = np.arctan(H / desL)
 beta = (beta_rad * 180) / np.pi
 
 
-
 #   Define TE constants
 
-# Manual Tuning
-#L = 20
-
-# Body Dimension Tuner
+#   Manual Tuning
+# L = 20
+#   Body Dimension Tuner
 L = 2*desL  # Max. Length (See User Menu)
 
 Rs = L * np.tan(beta_rad)  # Trailing Edge Parameter (See User Menu)
 
-# Manual Tuning
-#R1_multiplier = 0.5
-#W2_multiplier = 0.45
-
-# Body Dimension Tunner
+#   Manual Tuning
+# R1_multiplier = 0.5
+# W2_multiplier = 0.45
+#   Body Dimension Tuner
 R1_multiplier = 0.5
 W2_multiplier = (W/2)/Rs
 
 R1 = R1_multiplier * Rs  # Trailing Edge Parameter (See User Menu)
 W2 = W2_multiplier * Rs  # Trailing Edge Parameter (See User Menu)
 
+
 #   Resolution
 N = 500  # Leading Edge Resolution (500 or more is recommended!)
 N_l = 12  # Lower Surface Resolution
 N_up = 10  # Upper Surface Resolution
+
 
 #   Trailing Edge Curve
 a = -R1
